19df4b0d6e5473d6d93ad7328cbbf95070257589
Dora <dora.nacinovic99@gmail.com>
2024-06-18 15:22:35 +0200

    added auth and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  //   const navigate = useNavigate();
+
+  return (
+    <footer className="bg- footer p-10 text-base-content">
+      <aside>
+        <img className="size-16" src="./public/logo.png" alt="Event Booking logo" />
+        <br />
+        <p>
+          Event Booking
+          <br />
+          "From small gatherings to grand celebrations, <br /> our mission is to
+          make every event extraordinary."
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
