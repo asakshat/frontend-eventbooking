c2620c5f444fea2b6150e688cd86fac33c6b5143
Dora <dora.nacinovic99@gmail.com>
2024-06-25 14:02:07 +0200

    fixed the get directions layout and event/id maps layout

--- src/components/EventDirections.jsx
+++ src/components/EventDirections.jsx
@@ -8,17 +8,16 @@ import {
 function EventDirections({ coordinates }) {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-    const [directions, setDirections] = useState(null);
-    const [departPoint, setDepartPoint] = useState({
-        streetName: '',
-        city: '',
-        zipCode: '',
-        country: ''
-    });
+  const [directions, setDirections] = useState(null);
+  const [departPoint, setDepartPoint] = useState({
+    streetName: "",
+    city: "",
+    zipCode: "",
+    country: "",
+  });
 
-    const fullAddress = ` ${departPoint.streetName}, ${departPoint.city}, ${departPoint.zipCode}, ${departPoint.country}`;
-
-    const [error, setError] = useState(null);
+  const fullAddress = ` ${departPoint.streetName}, ${departPoint.city}, ${departPoint.zipCode}, ${departPoint.country}`;
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,10 +27,9 @@ function EventDirections({ coordinates }) {
     }));
   };
 
-    const calculateDirections = (method) => {
-    
-        const encodedAddress = encodeURIComponent(fullAddress);
-        const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`;
+  const calculateDirections = (method) => {
+    const encodedAddress = encodeURIComponent(fullAddress);
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`;
 
     fetch(geocodeUrl)
       .then((response) => response.json())
@@ -74,74 +72,90 @@ function EventDirections({ coordinates }) {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <form
-        className="event-creator"
-        data-theme="cyberpunk"
-      >
-        <label>
-          Street:
+    <div className="flex flex-row items-start space-x-4">
+      <form className="event-creator space-y-4 ml-5">
+        <label className="block">
+          Street: {""}
           <input
             type="text"
             name="streetName"
             value={departPoint.streetName}
             onChange={handleChange}
+            className="input"
           />
         </label>
-        <label>
-          Zip Code:
+        <label className="block">
+          Zip Code: {""}
           <input
             type="text"
-            className="w-11"
             name="zipCode"
             value={departPoint.zipCode}
             onChange={handleChange}
+            className="input"
           />
         </label>
-        <label>
-          City:
+        <label className="block">
+          City: {""}
           <input
             type="text"
             name="city"
             value={departPoint.city}
             onChange={handleChange}
+            className="input"
           />
         </label>
-        <label>
-          Country:
+        <label className="block">
+          Country: {""}
           <input
             type="text"
             name="country"
             value={departPoint.country}
             onChange={handleChange}
+            className="input"
           />
         </label>
-
-        <button type="submit" className="btn btn-primary w-20"
-        onClick={(e) => handleSubmit(e, "DRIVING")}>
-          By Car
-        </button>
-
-        <button type="submit" className="btn btn-primary w-20"
-        onClick={(e) => handleSubmit(e, "WALKING")}>
-          Walking
-        </button>
-
-        <button type="submit" className="btn btn-primary w-20"
-        onClick={(e) => handleSubmit(e, "TRANSIT")}>
-          Public Transport
-        </button>
+        <br />
+        <br />
+        <div className="space-x-4">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => handleSubmit(e, "DRIVING")}
+          >
+            By Car
+          </button>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => handleSubmit(e, "WALKING")}
+          >
+            Walking
+          </button>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => handleSubmit(e, "TRANSIT")}
+          >
+            Public Transport
+          </button>
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
       </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <LoadScript googleMapsApiKey={apiKey}>
-        <GoogleMap
-          zoom={10}
-          mapContainerStyle={{ width: "100%", height: "400px" }}
-          center={coordinates}
-        >
-          {directions && <DirectionsRenderer directions={directions} />}
-        </GoogleMap>
-      </LoadScript>
+      <div className="w-2/3 h-96">
+        <LoadScript googleMapsApiKey={apiKey}>
+          <GoogleMap
+            zoom={10}
+            mapContainerStyle={{
+              width: "100%",
+              height: "100%",
+              marginLeft: "20px",
+            }}
+            center={coordinates}
+          >
+            {directions && <DirectionsRenderer directions={directions} />}
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 }
