e7e5cb993cab2a7c4d41de31722999ca8b2fa7e2
Dora <dora.nacinovic99@gmail.com>
2024-06-26 19:44:51 +0200

    changed the create event layout

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -3,25 +3,24 @@ import GoogleMapInt from "./GoogleMapInt";
 import { useNavigate } from "react-router-dom";
 
 function CreateEvent() {
-    
-    const [responseMessage, setResponseMessage] = useState(''); 
-    const navigate = useNavigate();
-    const [address, setAddress] = useState({
-        streetName: '',
-        city: '',
-        zipCode: '',
-        country: ''
-    });
-    const fullAddress = `${address.streetName}, ${address.city}, ${address.zipCode}, ${address.country}`;
+  const [responseMessage, setResponseMessage] = useState(''); 
+  const navigate = useNavigate();
+  const [address, setAddress] = useState({
+    streetName: '',
+    city: '',
+    zipCode: '',
+    country: ''
+  });
+  const fullAddress = `${address.streetName}, ${address.city}, ${address.zipCode}, ${address.country}`;
 
-    const [name, setName] = useState('');
-    const [time, setTime] = useState('');
-    const [date, setDate] = useState('');
-    const [price, setPrice] = useState('');
-    const [description, setDescription] = useState('');
-    const [venue, setVenue] = useState('');
-    const [file, setFile] = useState(null); 
-    const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+  const [name, setName] = useState('');
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [venue, setVenue] = useState('');
+  const [file, setFile] = useState(null); 
+  const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
 
   const formatDate = (dateString) => {
     const [year, month, day] = dateString.split("-");
@@ -36,11 +35,10 @@ function CreateEvent() {
     }));
   };
 
-    // Get the coordinates of the address provided by the event creator
-    const getCoordinates = async () => {
-        const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-        const encodedAddress = encodeURIComponent(fullAddress);
-        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`;
+  const getCoordinates = async () => {
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    const encodedAddress = encodeURIComponent(fullAddress);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`;
 
     try {
       const response = await fetch(url);
@@ -59,199 +57,165 @@ function CreateEvent() {
     }
   };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
         
-        const coords = await getCoordinates();
-        const fetchURL = import.meta.env.VITE_FETCH_URL;
-        console.log(date);
-        console.log(time);
-        const formData = new FormData();
-        formData.append('title', name);
-        formData.append('date', formatDate(date));
-        formData.append('time', time);
-        formData.append('location', fullAddress);
-        formData.append('venue', venue);
-        formData.append('price', price);
-        formData.append('description', description);
-        formData.append('latitude', coords.lat);
-        formData.append('longitude', coords.lng);
+    const coords = await getCoordinates();
+    const fetchURL = import.meta.env.VITE_FETCH_URL;
+    const formData = new FormData();
+    formData.append('title', name);
+    formData.append('date', formatDate(date));
+    formData.append('time', time);
+    formData.append('location', fullAddress);
+    formData.append('venue', venue);
+    formData.append('price', price);
+    formData.append('description', description);
+    formData.append('latitude', coords.lat);
+    formData.append('longitude', coords.lng);
 
     if (file) {
       formData.append("image", file);
     }
 
-        try {
-            const response = await fetch(`${fetchURL}/api/event/create`, {
-                method: 'POST',
-                body: formData,
-                credentials: 'include'
-            });
-            const data = await response.json();
-            console.log(data);
-            setResponseMessage('Event created successfully!');
-            if (response.ok) {
-                navigate("/events")
-            }
-        } catch (error) {
-            console.error('Error submitting form:', error);
-            setResponseMessage('Error creating event. Please try again.');
-        }
-    };
+    try {
+      const response = await fetch(`${fetchURL}/api/event/create`, {
+        method: 'POST',
+        body: formData,
+        credentials: 'include'
+      });
+      const data = await response.json();
+      console.log(data);
+      setResponseMessage('Event created successfully!');
+      if (response.ok) {
+        navigate("/events")
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      setResponseMessage('Error creating event. Please try again.');
+    }
+  };
 
   return (
-    <div className="flex gap-5 p-10">
-      <form
-        className="w-full max-w-2xl grid grid-cols-1 md:grid-cols-2 gap-6"
-        data-theme="cyberpunk"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-3xl font-bold mb-4 md:col-span-2">Create Event</h1>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name:</span>
-          </label>
+    <div className="flex flex-col items-center p-10 min-h-screen">
+      <form className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5 bg-white text-black shadow-lg rounded-lg w-full max-w-5l" onSubmit={handleSubmit}>
+        <h1 className="col-span-1 md:col-span-2 text-3xl font-bold mb-4 text-lefy">Create Event</h1>
+        <label className="block mb-2">
+          Name:
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Date:</span>
+        </label>
+    
+        <div className="md:col-span-1 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <label className="block mb-2">
+            Date:
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="block mt-1 p-2 border rounded w-full"
+            />
           </label>
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="input input-bordered w-full"
-          />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Time:</span>
+         
+          <label className="block mb-2">
+            
+            Time:
+            <input
+              type="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              className="block mt-1 p-2 border rounded w-full"
+            />
           </label>
-          <input
-            type="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            className="input input-bordered w-full"
-          />
         </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Street and number:</span>
-          </label>
+        <label className="block mb-2 md:col-span-1">
+          Street:
           <input
             type="text"
             name="streetName"
             value={address.streetName}
             onChange={handleChange}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Zip Code:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-1">
+          Zip Code:
           <input
             type="text"
             name="zipCode"
             value={address.zipCode}
             onChange={handleChange}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">City:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-1">
+          City:
           <input
             type="text"
             name="city"
             value={address.city}
             onChange={handleChange}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Country:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-1">
+          Country:
           <input
             type="text"
             name="country"
             value={address.country}
             onChange={handleChange}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Venue:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-1">
+          Venue:
           <input
             type="text"
             name="venue"
             value={venue}
             onChange={(e) => setVenue(e.target.value)}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Price:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-1">
+          Price:
           <input
             type="number"
             name="price"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
-            className="input input-bordered w-full"
+            className="block mt-1 p-2 border rounded w-full"
           />
-        </div>
-
-        <div className="form-control md:col-span-2">
-          <label className="label">
-            <span className="label-text">Description:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-2">
+          Description:
           <textarea
-            className="textarea textarea-bordered w-full"
+            className="block w-full mt-1 p-2 border rounded"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
-        </div>
-
-        <div className="form-control md:col-span-2">
-          <label className="label">
-            <span className="label-text">Add an image:</span>
-          </label>
+        </label>
+        <label className="block mb-2 md:col-span-2">
+          Add an image:
           <input
             type="file"
             accept="image/*"
             onChange={(e) => setFile(e.target.files[0])}
-            className="file-input file-input-bordered w-full"
+            className="block w-full mt-1 p-2 border rounded"
           />
-        </div>
-
-        <div className="md:col-span-2">
-          <button className="btn btn-primary w-full" type="submit">
-            Submit
-          </button>
-        </div>
+        </label>
+        <button className="btn btn-primary col-span-1 md:col-span-2 mt-4 w-full" type="submit">
+          Submit
+        </button>
+        {responseMessage && (
+          <p className="mt-2 col-span-1 md:col-span-2 text-center text-red-500">{responseMessage}</p>
+        )}
       </form>
-      <div className="w-full max-w-lg">
+      <div className="w-full max-w-2xl mt-10">
         <GoogleMapInt
           lat={coordinates.lat}
           lng={coordinates.lng}
