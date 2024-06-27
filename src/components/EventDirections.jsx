04bd5ba5ec478588070b6f584bd323c9c426d4e3
asakshat <asakshat453@gmail.com>
2024-06-27 11:27:14 +0200

    added a rich text editor

--- src/components/EventDirections.jsx
+++ src/components/EventDirections.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 import {
 	GoogleMap,
-	LoadScript,
+	useJsApiLoader,
 	DirectionsRenderer,
 } from '@react-google-maps/api';
-  GoogleMap,
-  useJsApiLoader,
-  DirectionsRenderer,
-} from "@react-google-maps/api";
 
 function EventDirections({ coordinates }) {
 	const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-  const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: apiKey,
-  });
+	const { isLoaded, loadError } = useJsApiLoader({
+		googleMapsApiKey: apiKey,
+	});
 
 	const [directions, setDirections] = useState(null);
 	const [departPoint, setDepartPoint] = useState({
@@ -25,8 +20,7 @@ function EventDirections({ coordinates }) {
 		country: '',
 	});
 
-	const fullAddress = ` ${departPoint.streetName}, ${departPoint.city}, ${departPoint.zipCode}, ${departPoint.country}`;
-  const fullAddress = `${departPoint.streetName}, ${departPoint.city}, ${departPoint.zipCode}, ${departPoint.country}`;
+	const fullAddress = `${departPoint.streetName}, ${departPoint.city}, ${departPoint.zipCode}, ${departPoint.country}`;
 
 	const [error, setError] = useState(null);
 
@@ -86,13 +80,9 @@ function EventDirections({ coordinates }) {
 		e.preventDefault();
 		calculateDirections(method);
 	};
-  const handleSubmit = (e, method) => {
-    e.preventDefault();
-    calculateDirections(method);
-  };
 
-  if (loadError) return <div>Error loading maps</div>;
-  if (!isLoaded) return <div>Loading Maps</div>;
+	if (loadError) return <div>Error loading maps</div>;
+	if (!isLoaded) return <div>Loading Maps</div>;
 
 	return (
 		<div className="flex flex-row justify-evenly items-start">
@@ -165,63 +155,20 @@ function EventDirections({ coordinates }) {
 				{error && <p style={{ color: 'red' }}>{error}</p>}
 			</form>
 			<div className="w-2/3">
-				<LoadScript googleMapsApiKey={apiKey}>
-					<GoogleMap
-						zoom={10}
-						mapContainerStyle={{
-							width: '90%',
-							height: '430px',
-							marginLeft: '50px',
-						}}
-						center={coordinates}
-					>
-						{directions && <DirectionsRenderer directions={directions} />}
-					</GoogleMap>
-				</LoadScript>
+				<GoogleMap
+					zoom={10}
+					mapContainerStyle={{
+						width: '90%',
+						height: '430px',
+						marginLeft: '50px',
+					}}
+					center={coordinates}
+				>
+					{directions && <DirectionsRenderer directions={directions} />}
+				</GoogleMap>
 			</div>
 		</div>
 	);
-        <p className="mb-2">Choose your way of travel:</p>
-        <div className="flex gap-4">
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={(e) => handleSubmit(e, "DRIVING")}
-          >
-            By Car
-          </button>
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={(e) => handleSubmit(e, "WALKING")}
-          >
-            Walking
-          </button>
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={(e) => handleSubmit(e, "TRANSIT")}
-          >
-            Public Transport
-          </button>
-        </div>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </form>
-      <div className="w-2/3">
-        <GoogleMap
-          zoom={10}
-          mapContainerStyle={{
-            width: "90%",
-            height: "430px",
-            marginLeft: "50px",
-          }}
-          center={coordinates}
-        >
-          {directions && <DirectionsRenderer directions={directions} />}
-        </GoogleMap>
-      </div>
-    </div>
-  );
 }
 
 export default EventDirections;
