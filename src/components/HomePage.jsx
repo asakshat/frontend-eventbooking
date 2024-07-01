5a787fa4a4fd4fd0fbfa7af285f0257d03d3e1e7
asakshat <asakshat453@gmail.com>
2024-07-01 17:01:12 +0200

    a bit responsive

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,75 +1,78 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const images = [
-  'https://images.lifestyleasia.com/wp-content/uploads/sites/3/2022/04/07111030/tijs-van-leur-Qnlp3FCO2vc-unsplash-scaled.jpg',
-  // 'https://eadn-wc01-4383690.nxedge.io/wp-content/uploads/2023/11/5433200373_9b6d2c2257_o-scaled.jpg',
-  'https://s3-ap-southeast-2.amazonaws.com/ish-oncourse-scc/85edf77e-03d4-4d37-8171-130776d6d4d2?versionId=CyqL4UCaGm9Ld.YzcsTqQah7LpCs.E2m',
-  'https://liquidcapitalcorp.com/wp-content/uploads/2018/04/Prepare-for-a-conference.jpg',
-  'https://static01.nyt.com/images/2017/09/20/business/21ebiz1/21ebiz1-superJumbo.jpg',
-  'https://www.loghicconnect.com.au/wp-content/uploads/2020/05/Untitled-design-2023-03-16T112342.403.jpg',
-  'https://www.johntalk.com/wp-content/uploads/2018/06/The-Benefits-of-Working-Music-Festivals.jpg',
-  'https://www.nevadaart.org/wp-content/uploads/2018/11/2018_GalleryShot_AlteredLandscape1.jpg',
-  'https://www.walksinsiderome.com/wp-content/uploads/2023/01/italian-cooking-class-in-rome.jpg',
-  'https://www.johntalk.com/wp-content/uploads/2018/06/The-Benefits-of-Working-Music-Festivals.jpg',
+	'https://images.lifestyleasia.com/wp-content/uploads/sites/3/2022/04/07111030/tijs-van-leur-Qnlp3FCO2vc-unsplash-scaled.jpg',
+	'https://s3-ap-southeast-2.amazonaws.com/ish-oncourse-scc/85edf77e-03d4-4d37-8171-130776d6d4d2?versionId=CyqL4UCaGm9Ld.YzcsTqQah7LpCs.E2m',
+	'https://liquidcapitalcorp.com/wp-content/uploads/2018/04/Prepare-for-a-conference.jpg',
+	'https://static01.nyt.com/images/2017/09/20/business/21ebiz1/21ebiz1-superJumbo.jpg',
+	'https://www.loghicconnect.com.au/wp-content/uploads/2020/05/Untitled-design-2023-03-16T112342.403.jpg',
+	'https://www.johntalk.com/wp-content/uploads/2018/06/The-Benefits-of-Working-Music-Festivals.jpg',
+	'https://www.nevadaart.org/wp-content/uploads/2018/11/2018_GalleryShot_AlteredLandscape1.jpg',
+	'https://www.walksinsiderome.com/wp-content/uploads/2023/01/italian-cooking-class-in-rome.jpg',
+	'https://www.johntalk.com/wp-content/uploads/2018/06/The-Benefits-of-Working-Music-Festivals.jpg',
 ];
 
 const Homepage = () => {
-  const navigate = useNavigate();
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+	const navigate = useNavigate();
+	const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 6000); 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+		}, 6000);
 
-    return () => clearInterval(interval); 
-  }, []);
+		return () => clearInterval(interval);
+	}, []);
 
-  return (
-    <div className="flex min-h-screen">
-     
-      <div className="flex flex-col justify-center w-3/5 p-10">
-        <h1 className="text-8xl font-bold">Event Booking</h1>
-        <p className="text-2xl font-bold py-10">
-          Discover exciting opportunities and stay connected with the latest happenings.
-        </p>
-        <div className="flex flex-col space-y-4">
-          <button
-            className="btn btn-secondary dark:btn-primary"
-            onClick={() => navigate("/events")}
-          >
-            Events
-          </button>
-          <button
-            className="btn btn-primary dark:btn-secondary"
-            onClick={() => navigate("/join")}
-          >
-            Join Us
-          </button>
-          <button
-            className="btn bg-neutral-500"
-            onClick={() => navigate("/about")}
-          >
-            About Us
-          </button>
-        </div>
-      </div>
-      
+	return (
+		<div className="flex flex-col md:flex-row min-h-screen">
+			<div className="flex flex-col justify-center w-full md:w-3/5 p-4 md:p-10">
+				<h1 className="text-2xl md:text-3xl font-bold">Event Booking</h1>
+				<p className="text-xl md:text-2xl font-bold py-5 md:py-10">
+					Discover exciting opportunities and stay connected with the latest
+					happenings.
+				</p>
+				<div className="flex flex-col space-y-4">
+					<button
+						className="btn btn-secondary dark:btn-primary"
+						onClick={() => navigate('/events')}
+					>
+						Events
+					</button>
+					<button
+						className="btn btn-primary dark:btn-secondary"
+						onClick={() => navigate('/join')}
+					>
+						Join Us
+					</button>
+					<button
+						className="btn bg-neutral-500"
+						onClick={() => navigate('/about')}
+					>
+						About Us
+					</button>
+				</div>
+			</div>
 
-      <div className="w-2/5 h-screen relative">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${
-              index === currentImageIndex ? "opacity-100" : "opacity-0"
-            }`}
-            style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center', zIndex: index === currentImageIndex ? 10 : 1 }}
-          />
-        ))}
-      </div>
-    </div>
-  );
+			<div className="w-full md:w-2/5 h-64 md:h-screen relative">
+				{images.map((image, index) => (
+					<div
+						key={index}
+						className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${
+							index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+						}`}
+						style={{
+							backgroundImage: `url(${image})`,
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+							zIndex: index === currentImageIndex ? 10 : 1,
+						}}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,138 +1,138 @@
-import { useContext, useState, useEffect } from "react";
-import { MdOutlineLogin } from "react-icons/md";
-import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png";
-import DropdownMenu from "./DropdownMenu";
-import { UserContext } from "./UserContext";
-import { data } from "autoprefixer";
+import { useContext, useState, useEffect } from 'react';
+import { MdOutlineLogin } from 'react-icons/md';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import DropdownMenu from './DropdownMenu';
+import { UserContext } from './UserContext';
 
 export default function Navbar() {
-  const navigate = useNavigate();
-  const { user } = useContext(UserContext);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [allEvents, setAllEvents] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
+	const navigate = useNavigate();
+	const { user } = useContext(UserContext);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [suggestions, setSuggestions] = useState([]);
+	const [allEvents, setAllEvents] = useState([]);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
 
-  const navigateToAuthForm = () => {
-    navigate("/auth");
-  };
+	const navigateToAuthForm = () => {
+		navigate('/auth');
+	};
 
-  useEffect(() => {
-    const fetchURL = import.meta.env.VITE_FETCH_URL;
-    fetch(`${fetchURL}/api/event`)
-      .then((response) => response.json())
-      .then((data) => setAllEvents(data.events || []))
-      .catch((error) => console.error("Failed to retrieve data:", error));
-  }, []);
+	useEffect(() => {
+		const fetchURL = import.meta.env.VITE_FETCH_URL;
+		fetch(`${fetchURL}/api/event`)
+			.then((response) => response.json())
+			.then((data) => setAllEvents(data.events || []))
+			.catch((error) => console.error('Failed to retrieve data:', error));
+	}, []);
 
-  useEffect(() => {
-    if (searchTerm) {
-      const filteredEvents = allEvents.filter((event) =>
-        event.Title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setSuggestions(filteredEvents);
-    } else {
-      setSuggestions([]);
-    }
-  }, [searchTerm, allEvents]);
+	useEffect(() => {
+		if (searchTerm) {
+			const filteredEvents = allEvents.filter((event) =>
+				event.Title.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+			setSuggestions(filteredEvents);
+		} else {
+			setSuggestions([]);
+		}
+	}, [searchTerm, allEvents]);
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-  const handleSuggestionClick = (event) => {
-    navigate(`/event/${event.ID}`, { state: event });
-    setSearchTerm("");
-    setSuggestions([]);
-  };
+	const handleSearchChange = (event) => {
+		setSearchTerm(event.target.value);
+	};
+	const handleSuggestionClick = (event) => {
+		navigate(`/event/${event.ID}`, { state: event });
+		setSearchTerm('');
+		setSuggestions([]);
+	};
 
-  const userTrunc =
-    user && user.user && user.user.Email
-      ? user.user.Email.split("@")[0].substring(0, 1).toUpperCase()
-      : "";
+	const userTrunc =
+		user && user.user && user.user.Email
+			? user.user.Email.split('@')[0].substring(0, 1).toUpperCase()
+			: '';
 
-  return (
-    <div className="navbar bg-neutral-600 gap-4 relative z-50">
-      <div className="flex-1">
-        <Link to="/">
-          <img
-            className="w-24 h-24 p-1 hover:cursor-pointer"
-            src={logo}
-            alt="Event Booking logo"
-          />
-        </Link>
-      </div>
-      {user && <DropdownMenu />}
-      <div className="flex-none gap-2 relative">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search for events"
-            className="input input-bordered w-24 md:w-auto"
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-          {suggestions.length > 0 && (
-            <div className="absolute top-full mt-1 w-full bg-white border rounded shadow-lg z-50">
-              {suggestions.map((suggestion) => (
-                <div
-                  key={suggestion.ID}
-                  className="p-2 cursor-pointer hover:bg-gray-200"
-                  onClick={() => handleSuggestionClick(suggestion)}
-                >
-                  {suggestion.Title}
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        {userTrunc && (
-          <div
-            className="avatar placeholder relative"
-            onClick={() => toggleDropdown()}
-            role="button"
-            tabIndex="0"
+	return (
+		<div className="navbar bg-neutral-600 gap-4 relative z-50">
+			<div className="flex-1">
+				<Link to="/">
+					<img
+						className="w-24 h-24 p-1 hover:cursor-pointer"
+						src={logo}
+						alt="Event Booking logo"
+					/>
+				</Link>
+			</div>
+			{user && <DropdownMenu />}
+			<div className="flex-none gap-2 relative">
+				<div className="form-control">
+					<input
+						type="text"
+						placeholder="Search for events"
+						className="input input-bordered w-24 md:w-auto"
+						value={searchTerm}
+						onChange={handleSearchChange}
+					/>
+					{suggestions.length > 0 && (
+						<div className="absolute top-full mt-1 w-full bg-white border rounded shadow-lg z-50">
+							{suggestions.map((suggestion) => (
+								<div
+									key={suggestion.ID}
+									className="p-2 cursor-pointer hover:bg-gray-200"
+									onClick={() => handleSuggestionClick(suggestion)}
+								>
+									{suggestion.Title}
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+				{userTrunc && (
+					<div
+						className="avatar placeholder relative"
+						onClick={() => toggleDropdown()}
+						role="button"
+						tabIndex="0"
+					>
+						<div className="bg-neutral text-neutral-content w-14 rounded-full">
+							<span className="text-xl">{userTrunc}</span>
+						</div>
+						<div className=" overflow-scroll ">
+							{isOpen && <DropdownMenu isOpen={true} toggle={true} />}
+						</div>
+					</div>
+				)}
 
-          >
-            <div className="bg-neutral text-neutral-content w-14 rounded-full">
-              <span className="text-xl">{userTrunc}</span>
-            </div>
-            <div className=" overflow-scroll ">{isOpen && <DropdownMenu isOpen={true} toggle={true} />}</div>
-          </div>
-        )}
-
-        <label className="swap swap-rotate">
-          <input type="checkbox" className="theme-controller" value="night" />
-          <svg
-            className="swap-off fill-current text-black dark:text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-          </svg>
-          <svg
-            className="swap-on fill-current w-7 h-7 text-black dark:text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-          </svg>
-        </label>
-        <div>
-          {!user && (
-            <button
-              onClick={navigateToAuthForm}
-              className="btn btn-primary btn-circle"
-            >
-              <MdOutlineLogin className="w-5 h-5 text-black dark:text-white" />
-            </button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+				<label className="swap swap-rotate">
+					<input type="checkbox" className="theme-controller" value="night" />
+					<svg
+						className="swap-off fill-current text-black dark:text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+					>
+						<path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+					</svg>
+					<svg
+						className="swap-on fill-current w-7 h-7 text-black dark:text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+					>
+						<path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+					</svg>
+				</label>
+				<div>
+					{!user && (
+						<button
+							onClick={navigateToAuthForm}
+							className="btn btn-primary btn-circle"
+						>
+							<MdOutlineLogin className="w-5 h-5 text-black dark:text-white" />
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
